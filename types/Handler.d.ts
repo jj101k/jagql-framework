db5278ca9c43fe45312552cfd859a02e4afdc6e0
Jim Driscoll <jim.a.driscoll@gmail.com>
2025-08-19 21:52:55 -0500

    Compat: restore Handler.d.ts

---
+++ types/Handler.d.ts
@@ -0,0 +1,12 @@
+/**
+ * @deprecated Please load from top level
+ */
+export * from "./CallbackHandler"
+/**
+ * @deprecated Please load from top level
+ */
+export * from "./JsonApiRequest"
+/**
+ * @deprecated use CallbackHandler
+ */
+export { CallbackHandler as Handler } from "./CallbackHandler"
