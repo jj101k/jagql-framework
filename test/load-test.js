8db69361937a7600836fa4637244e51c61d40e52
Jim Driscoll <jim.a.driscoll@gmail.com>
2025-08-23 12:48:49 -0500

    Split out the load test for better verification

---
+++ test/load-test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const assert = require('assert')
+const helpers = require('./helpers.js')
+const jsonApiTestServer = require('../example/server.js')
+
+describe("Load test", () => {
+  it("can load 100k objects in acceptable time", async () => {
+    const { expect } = await import('chai')
+    const start = new Date()
+    const {err, res, json} = await helpers.requestAsync({
+      method: "GET",
+      url: "http://localhost:16999/rest/notes?page[limit]=1000000"
+    })
+    const finish = new Date()
+    const elapsedMs = finish.valueOf() - start.valueOf()
+    assert.strictEqual(err, null)
+    // Note that 100,000 * 5us = 500,000us = 500ms
+    expect(elapsedMs).to.be.lessThan(500, "Elapsed time is <5us per item")
+    const vjson = helpers.validateJson(json)
+
+    assert.strictEqual(res.statusCode, 200, "Expecting 200 OK")
+    assert.deepEqual(vjson.included, [ ], "Response should have no included resources")
+    assert.strictEqual(vjson.data.length, 100_000, "Response should contain all resources")
+  })
+
+  before(() => {
+    jsonApiTestServer.start()
+  })
+  after(() => {
+    jsonApiTestServer.close()
+  })
+})
